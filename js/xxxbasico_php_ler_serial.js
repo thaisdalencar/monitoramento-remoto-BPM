61812af92ed5f8f1aa0dd6ae419695bc5ea262f3
thaisdalencar <thaisdalencar@gmail.com>
2015-04-23 15:24:29 -0300

    comentarios no js

--- js/xxxbasico_php_ler_serial.js
+++ js/xxxbasico_php_ler_serial.js
@@ -10,7 +10,7 @@ var Peak_Magnitude;
 var Minimum_Peak_Separation = 50;
 var Peak_Threshold_Factor = 80;
 var Moving_Average_Num = 10;
-var Num_Samples = 650;// não sei pq, mas quando maior do que 600, ocorre menos underfined
+var Num_Samples = 600;
 var valores = [];
 var Pulse_Rate;
 var ZeroFlag;
@@ -25,10 +25,13 @@ var Peak3;
 var PR1;
 var PR2;
 var BPM;
+var bpm_final;
 
 
 function ler_batimentos(ADC_Value){
+	console.log(Array.isArray(ADC_Value));
 	RemoveDC(ADC_Value);
+	console.log("ADC_Range = "+ADC_Range);
 	if(ADC_Range < 50){ /*indica que é so ruido*/
 		/*se a diferença entre o pico e o vale for menor que 50, todo o array receberá valor 0, pois diferença menor que 50 idica que não tem pulsação é so ruido*/
 	    ZeroFlag = 1;
@@ -41,6 +44,7 @@ function ler_batimentos(ADC_Value){
 	  	FilterData(ADC_Value);
 	  	ComputeHeartRate(ADC_Value);
 	}
+	/*$("#valor").append("</br>"+BPM);*/
 	atualizar_bpm(BPM);
 }
 function atualizar_bpm(BPM){
@@ -53,6 +57,9 @@ function RemoveDC(ADC_Value){/*Encontra a diferença entre o pico e o vale*/
 	Find_Minima(ADC_Value,0);
     Find_Peak(ADC_Value,0);
     ADC_Range = Peak_Magnitude-Minima;
+    console.log("Peak_Magnitude = "+Peak_Magnitude);
+    console.log("Minima = "+Minima);
+
 /*    Normatiza o array de amostra, subtrai de cada amostra o valor minimo encontrado no array, fazendo com q toda a onda desça para o eixo x*/
     for (var i = 0; i < Num_Samples; i++){ 
 	    ADC_Value[i] = ADC_Value[i] - Minima;
@@ -79,11 +86,12 @@ function Find_Minima(ADC_Value, num){ /*Encontra o valor minimo entre o array de
 function ZeroData(ADC_Value){ /*todos os valores so array recebem 0*/
     for (var i = 0; i < Num_Samples; i++){
         ADC_Value[i] = 0;     
-  	}
+  }
 }
 function ScaleData(ADC_Value){
  /*padroniza os valores do aray, evitando por exemplo uma sequencia x 2x 3x 99x 5x, ai ele tipo tipa a media e realoca o array*/
 	Find_Peak(ADC_Value,0);
+	console.log("minima segunda = "+Minima);
 	Range = Peak_Magnitude - Minima;
 	// escala valores entre 1 e 1023 
 	for (var i = 0; i < Num_Samples; i++){
@@ -91,6 +99,8 @@ function ScaleData(ADC_Value){
 	}
 	Find_Peak(ADC_Value,0);
 	Find_Minima(ADC_Value,0);  
+	console.log("Peak_Magnitude = "+Peak_Magnitude);
+    console.log("Minima terceira = "+Minima);
 }
 function FilterData(ADC_Value){
 	Num_Points = 2*Moving_Average_Num+1;
@@ -103,12 +113,16 @@ function FilterData(ADC_Value){
 	} 
 	Find_Peak(ADC_Value,Moving_Average_Num);
 	Find_Minima(ADC_Value,Moving_Average_Num);  
+	console.log("Peak Magnitude2= "+ Peak_Magnitude + ", Minima2 = "+ Minima);
 }
 function ComputeHeartRate(ADC_Value){
-	Find_Peak(ADC_Value, Moving_Average_Num);//procura pico entre o indice 10 e 589
-	Find_Minima(ADC_Value, Moving_Average_Num);//procura minimo entre o indice 10 e 589
-	Range = Peak_Magnitude - Minima;//calcula a diferença
-	Peak_Threshold = Peak_Magnitude*Peak_Threshold_Factor;//limiar de pico: valor maximo para o valor de pico
+	Find_Peak(ADC_Value, Moving_Average_Num);
+	Find_Minima(ADC_Value, Moving_Average_Num);
+	console.log("Peak Magnitude3= "+ Peak_Magnitude + ", Minima = "+ Minima); 
+	Range = Peak_Magnitude - Minima;
+	console.log("Range = "+ Range); 
+	Peak_Threshold = Peak_Magnitude*Peak_Threshold_Factor;
+	console.log("Peak_Threshold = "+ Peak_Threshold); 
 	Peak_Threshold = Peak_Threshold/100;
 	/*Agora vai buscar 3 picos consecutivos*/
 	Peak1 = 0;
@@ -118,13 +132,13 @@ function ComputeHeartRate(ADC_Value){
 	Index2 = 0;
 	Index3 = 0;
 	/*Busca o primeiro pico*/
-	for (var j = Moving_Average_Num; j < Num_Samples-Moving_Average_Num; j++){//procura os indices entre 10 e 589
+	for (var j = Moving_Average_Num; j < Num_Samples-Moving_Average_Num; j++){
 	  if(ADC_Value[j] >= ADC_Value[j-1] && ADC_Value[j] > ADC_Value[j+1] && ADC_Value[j] > Peak_Threshold && Peak1 == 0){
 	       Peak1 = ADC_Value[j];
 	       Index1 = j; 
 	  }
 	  /*Procura pelo segundo pico, que esteja no minimo a uma distancia de 10 amostras*/
-	  if(Peak1 > 0 && j > (Index1+Minimum_Peak_Separation) && Peak2 == 0){//Se j for superior ao indice do pico 1 mais 50 indices de separação
+	  if(Peak1 > 0 && j > (Index1+Minimum_Peak_Separation) && Peak2 == 0){
 	     if(ADC_Value[j] >= ADC_Value[j-1] && ADC_Value[j] > ADC_Value[j+1] && 
 	     ADC_Value[j] > Peak_Threshold){
 	        Peak2 = ADC_Value[j];
@@ -137,41 +151,36 @@ function ComputeHeartRate(ADC_Value){
 	     if(ADC_Value[j] >= ADC_Value[j-1] && ADC_Value[j] > ADC_Value[j+1] && 
 	     ADC_Value[j] > Peak_Threshold){
 	        Peak3 = ADC_Value[j];
-	        Index3 = j; //Index3 é setado em 0 quando apos vasculhar todo array nenhum se encaixa a condição
+	        Index3 = j; 
 	     } 
 	  } /*Peak2 > 0*/
 
 	}  
-	console.log("Index1 = "+Index1+", Index2 = "+Index2, "Index3 = "+Index3);
 	PR1 = (Index2-Index1)*Sampling_Time; // In milliseconds
 	PR2 = (Index3-Index2)*Sampling_Time;
 	console.log("PR1 = "+PR1+", PR2 = "+PR2);
-	var diferenca = (PR1-PR2);
-	/*soma = Math.abs(soma);*/
-	console.log("PR1-PR2 = "+diferenca);
+	
 	/*Se Math.abs(PR1-PR2) não for maior que 100 ele repete o ultimo valor do BPM*/
-	/*isso ocorre quando um dos index é 0*/
 	if(PR1 > 0 && (Math.abs(PR1-PR2)) < 100){
 		Pulse_Rate = (PR1+PR2)/2;
 		BPM = 60000/Pulse_Rate; // In BPM
-		console.log("BPM = "+BPM);
+		console.log("Index2= "+ Index2 + ", Index1 = "+ Index1+", PulseRate= "+Pulse_Rate); 
+		console.log("Peak Magnitude= "+ Peak_Magnitude + ", Minima = "+ Minima); 
+		console.log("BPM= "+ BPM); 
 	}
 }
 
 $(document).ready(function(){
-	var cont = 0;
 	console.log("iniciou");
 	setInterval(function(){
 	  	$.ajax({
 	        type: "GET",
 	        url: "return_of_python.php",
 	        success: function (data){
-	        	console.log(cont); 
 	        	console.log(data.length); 
-      			ler_batimentos(data);  
-      			cont = cont + 1;      	
+      			ler_batimentos(data);        	
 			},
 			dataType: "json"
 	    });
-	}, 4500);	
+	}, 3000);	
 });
